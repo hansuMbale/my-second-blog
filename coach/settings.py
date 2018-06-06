29d7c799dfb7fe7065f6b2edc5f02e984060aadc
nazirini <nazirinisiraji@gmail.com>
2018-06-06 16:58:10 +0300

    My Django Girls app, extention commits

--- coach/settings.py
+++ coach/settings.py
@@ -77,10 +77,14 @@ WSGI_APPLICATION = 'coach.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'djangogirls',
+'USER': 'name',
+'PASSWORD': '',
+'HOST': 'localhost',
+'PORT': '',
+}
 }
 
 
